f2416283f094030433e3a65332acfcbf2fd9b0f7
Sergio Rosa <scarlosjazz@gmail.com>
2020-11-09 09:24:56 -0500

    client side fetching

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,33 @@
 
+import { useState,useEffect } from 'react';
 import { Title } from '../styles/pages/Home';
 
+interface IProduct {
+  id: string,
+  title: string,
+}
 export default function Home() {
+  const [recommendedProducts, setRecommendedProducts] = useState<IProduct[]>([]);
+
+  useEffect(()=> {
+    fetch('http://localhost:3333/recommended').then(response => {
+      response.json().then(data => {
+        setRecommendedProducts(data);
+      })
+    })
+  },[]);
   return (
     <div>
-      <Title>Hello, Sergio!!</Title>
+      <Title>Products</Title>
+      <ul>
+        {recommendedProducts.map(recommendedProducts => {
+          return (
+            <li key={recommendedProducts.id}>
+              {recommendedProducts.title}
+            </li>
+          )
+        })}
+      </ul>
     </div>
 
   )
